f1f19714beef855729b7ab0dd00f4eb8107571db
Jonathan Rhodes <jon.rhodes@qut.edu.au>
2024-03-29 10:41:13 +1000

    Tidy up circular plot code and delete alluvial plot code

--- analysis.R
+++ analysis.R
@@ -392,43 +392,7 @@ write_csv(Actorss, "actors_counts.csv")
 
 # create some plots
 
-# get alluvial plot of nexus challenges versus nexus elements
-
-# get all combinations of nexus challenges and nexus elements for each study
-Challenges_Nexuses <- Data_Select_Split$NChallenge %>% map2(Data_Select_Split$Nexus,
-                       .f = get_challenge_nexus) %>% map(.f = function(x) {
-                         y <- length(unique(x$Nexus));
-                         return(x %>% mutate(NumNexus = as.character(y)))})
-# row bind list and remove NA values
-Challenges_Nexuses <- do.call(bind_rows, Challenges_Nexuses) %>%
-      filter(!is.na(NChallenge))
-# group by challenges and nexus elements and calculatye the frequency of each unique
-# combination
-Challenges_Nexuses <- Challenges_Nexuses %>% group_by(NChallenge, Nexus, NumNexus) %>%
-    summarize(Freq = n()) %>% ungroup()
-
-# create alluvial plot
-# Define the color palette
-custom_palette <- colorRampPalette(c("#D9AA80", "#B65719", "#791E32"))(4)
-#custom_palette1 <- scale_fill_gradient(low = "#D9AA80", high = "#791E32")
-#custom_palette2 <- scale_fill_gradient2(low = "#D9AA80", mid = "#B65719", high = "#791E32")
-#custom_palette3 <-  scale_fill_gradientn(colors = c("#D9AA80", "#B65719", "#791E32"))
-
-ggplot(Challenges_Nexuses, aes(y = Freq, axis1 = NChallenge, axis2 = Nexus)) +
-    geom_alluvium(aes(fill = NumNexus)) +
-    geom_stratum() +
-    geom_text(stat = "stratum",
-            aes(label = after_stat(stratum))) +
-    scale_x_discrete(limits = c("Nexus Challenges", "Nexus Elements"),
-                   expand = c(0.05, 0.05)) +
-    scale_fill_manual(values = color_palette) +
-  theme_void() +
-    guides(fill = guide_legend(title = "Number of Nexus Elements")) +
-    theme(legend.position = "bottom")
-
-ggsave("challenges_nexus_alluvial.jpg", width = 20, height = 10, units = "cm")
-
-# create circular plot
+# get circular plot of nexus challenges versus nexus elements
 
 # code modified from https://r-graph-gallery.com/299-circular-stacked-barplot.html
 
@@ -501,7 +465,7 @@ p <- ggplot(Challenges_Nexus) +
     axis.title = element_blank(),
     panel.grid = element_blank(),
     plot.margin = unit(rep(-1, 4), "cm")
-  ) + labs(color = "Number of Nexus Elements") +
+  ) + guides(fill = guide_legend(title = "Number of Nexus Elements")) +
 
   coord_polar() +
 
@@ -513,95 +477,7 @@ p <- ggplot(Challenges_Nexus) +
   geom_text(data = base_data, aes(x = title, y = -18, label = NChallenge), hjust=c(1, 1, 0.75, 0, 0), colour = "black", alpha = 0.8, size = 4, fontface = "bold", inherit.aes = FALSE)
 
 # Save figure
-ggsave(p, file = "challenges_nexus_circular.jpg", width = 25, height = 20, units = "cm")
-
-
-
-
-
-
-# Create dataset
-data <- data.frame(
-  individual=paste( "Mister ", seq(1,60), sep=""),
-  group=c( rep('A', 10), rep('B', 30), rep('C', 14), rep('D', 6)) ,
-  value1=sample( seq(10,100), 60, replace=T),
-  value2=sample( seq(10,100), 60, replace=T),
-  value3=sample( seq(10,100), 60, replace=T)
-)
-
-# Transform data in a tidy format (long format)
-data <- data %>% gather(key = "observation", value="value", -c(1,2))
-
-# Set a number of 'empty bar' to add at the end of each group
-empty_bar <- 2
-nObsType <- nlevels(as.factor(data$observation))
-to_add <- data.frame( matrix(NA, empty_bar*nlevels(as.factor(data$group))*nObsType, ncol(data)) )
-colnames(to_add) <- colnames(data)
-to_add$group <- rep(levels(as.factor(data$group)), each=empty_bar*nObsType )
-data <- rbind(data, to_add)
-data <- data %>% arrange(group, individual)
-data$id <- rep( seq(1, nrow(data)/nObsType) , each=nObsType)
-
-# Get the name and the y position of each label
-label_data <- data %>% group_by(id, individual) %>% summarize(tot=sum(value))
-number_of_bar <- nrow(label_data)
-angle <- 90 - 360 * (label_data$id-0.5) /number_of_bar     # I substract 0.5 because the letter must have the angle of the center of the bars. Not extreme right(1) or extreme left (0)
-label_data$hjust <- ifelse( angle < -90, 1, 0)
-label_data$angle <- ifelse(angle < -90, angle+180, angle)
-
-# prepare a data frame for base lines
-base_data <- data %>%
-  group_by(group) %>%
-  summarize(start=min(id), end=max(id) - empty_bar) %>%
-  rowwise() %>%
-  mutate(title=mean(c(start, end)))
-
-# prepare a data frame for grid (scales)
-grid_data <- base_data
-grid_data$end <- grid_data$end[ c( nrow(grid_data), 1:nrow(grid_data)-1)] + 1
-grid_data$start <- grid_data$start - 1
-grid_data <- grid_data[-1,]
-
-# Make the plot
-p <- ggplot(data) +
-
-  # Add the stacked bar
-  geom_bar(aes(x=as.factor(id), y=value, fill=observation), stat="identity", alpha=0.5) +
-  scale_fill_viridis(discrete=TRUE) +
-
-  # Add a val=100/75/50/25 lines. I do it at the beginning to make sur barplots are OVER it.
-  geom_segment(data=grid_data, aes(x = end, y = 0, xend = start, yend = 0), colour = "grey", alpha=1, size=0.3 , inherit.aes = FALSE ) +
-  geom_segment(data=grid_data, aes(x = end, y = 50, xend = start, yend = 50), colour = "grey", alpha=1, size=0.3 , inherit.aes = FALSE ) +
-  geom_segment(data=grid_data, aes(x = end, y = 100, xend = start, yend = 100), colour = "grey", alpha=1, size=0.3 , inherit.aes = FALSE ) +
-  geom_segment(data=grid_data, aes(x = end, y = 150, xend = start, yend = 150), colour = "grey", alpha=1, size=0.3 , inherit.aes = FALSE ) +
-  geom_segment(data=grid_data, aes(x = end, y = 200, xend = start, yend = 200), colour = "grey", alpha=1, size=0.3 , inherit.aes = FALSE ) +
-
-  # Add text showing the value of each 100/75/50/25 lines
-  ggplot2::annotate("text", x = rep(max(data$id),5), y = c(0, 50, 100, 150, 200), label = c("0", "50", "100", "150", "200") , color="grey", size=6 , angle=0, fontface="bold", hjust=1) +
-
-  ylim(-150,max(label_data$tot, na.rm=T)) +
-  theme_minimal() +
-  theme(
-    legend.position = "none",
-    axis.text = element_blank(),
-    axis.title = element_blank(),
-    panel.grid = element_blank(),
-    plot.margin = unit(rep(-1,4), "cm")
-  ) +
-  coord_polar() +
-
-  # Add labels on top of each bar
-  geom_text(data=label_data, aes(x=id, y=tot+10, label=individual, hjust=hjust), color="black", fontface="bold",alpha=0.6, size=5, angle= label_data$angle, inherit.aes = FALSE ) +
-
-  # Add base line information
-  geom_segment(data=base_data, aes(x = start, y = -5, xend = end, yend = -5), colour = "black", alpha=0.8, size=0.6 , inherit.aes = FALSE )  +
-  geom_text(data=base_data, aes(x = title, y = -18, label=group), hjust=c(1,1,0,0), colour = "black", alpha=0.8, size=4, fontface="bold", inherit.aes = FALSE)
-
-# Save at png
-ggsave(p, file="output.png", width=10, height=10)
-
-
-
+ggsave(p, file = "challenges_nexus_circular.jpg", width = 30, height = 20, units = "cm")
 
 # create heatmap plots for figure on governance, policy instruments, and nexus challenges
 
